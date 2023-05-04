64dd9cafda701015f83c0797a48e3829598e032f
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-05-04 00:27:02 -0700

    Be able to rework the portfolio to use the more relavent information
    Be able to rework the portfolio to use the more relavent information

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,12 +2,6 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
   return (
@@ -18,69 +12,20 @@ function Home2() {
             MORE <span className="purple"> ABOUT </span> ME
           </h1>
           <p>
-            Skilled in the development of software and web applications using
+            Skilled in software and web application development using
             technologies such as React and JavaScript. Experienced in creating
             educational games and tools, with a focus on cross-platform
             development. Responsibilities include designing, building, testing,
-            and debugging software. Constantly learning and improving skills and
-            knowledge in the field through professional development. Aiming to
-            create user-friendly and easy-to-understand products.
+            and debugging software. Actively exploring new technologies,
+            including machine learning engineering, to enhance proficiency.
+            Aiming to create user-friendly and easy-to-understand web-based
+            products.
           </p>
         </Col>
         <Col md={4} className="myAvtar">
           <Tilt>
             <img src={myImg} className="img-fluid" alt="avatar" />
           </Tilt>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={12} className="home-about-social">
-          <h1>FIND ME ON</h1>
-          <p>
-            Feel free to <span className="purple">connect </span>with me
-          </p>
-          <ul className="home-about-social-links">
-            <li className="social-icons">
-              <a
-                href="https://github.com/soumyajit4419"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour  home-social-icons"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://twitter.com/Soumyajit4419"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour  home-social-icons"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
-                target="blank"
-                rel="noreferrer"
-                className="icon-colour  home-social-icons"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/soumyajit4419"
-                target="_blank"
-                rel="noreferrer"
-                className="icon-colour home-social-icons"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
         </Col>
       </Row>
     </Container>
